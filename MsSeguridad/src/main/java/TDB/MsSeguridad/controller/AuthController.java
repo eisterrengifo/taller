664ac431ca169fb3094d8f054ac5430af41bc8c3
AichingerD <diogenes.a56@gmail.com>
2023-11-15 10:49:25 -0500

    Subiendo_Metodo_FindById
    busca por ID

--- MsSeguridad/src/main/java/TDB/MsSeguridad/controller/AuthController.java
+++ MsSeguridad/src/main/java/TDB/MsSeguridad/controller/AuthController.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import java.util.Optional;
 
 import TDB.MsSeguridad.services.AuthService;
 
@@ -39,5 +43,18 @@ public class AuthController {
      public void eliminarUsuario(@PathVariable int idUsuario) {
           authService.Delete(idUsuario);      
      }
+     @GetMapping("/{idUsuario}")
+public ResponseEntity<UsuarioModel> obtenerUsuarioPorId(@PathVariable String idUsuario) {
+    try {
+        int id = Integer.parseInt(idUsuario);
+        Optional<UsuarioModel> usuario = authService.FindById(id);
+        return usuario
+                .map(value -> ResponseEntity.ok().body(value))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    } catch (NumberFormatException e) {
+        // Manejar el caso en que el idUsuario no sea un número válido
+        return ResponseEntity.badRequest().build();
+    }
+}
  
 }
--- MsSeguridad/src/main/java/TDB/MsSeguridad/services/AuthService.java
+++ MsSeguridad/src/main/java/TDB/MsSeguridad/services/AuthService.java
@@ -1,7 +1,7 @@
 package TDB.MsSeguridad.services;
 
 import java.util.List;
-// import java.util.Optional;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,5 +27,8 @@ public class AuthService {
     public void Delete(int idUsuario) {
         authRepository.deleteById(idUsuario);
     }
+    public Optional<UsuarioModel> FindById(int idUsuario) {
+        return authRepository.findById(idUsuario);
+     }
    
 }
